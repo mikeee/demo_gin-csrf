37fb897bde6579958ef4c961f70d42d30ee770e1
mikeee <hey@mike.ee>
2019-04-20 17:27:51 +0100

    implemented gin routing and handling with csrf

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/utrack/gin-csrf"
+)
+
+func main() {
+	r := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+	r.Use(csrf.Middleware(csrf.Options{
+		Secret: "secret123",
+		ErrorFunc: func(c *gin.Context) {
+			c.String(400, "CSRF token mismatch")
+			c.Abort()
+		},
+	}))
+
+	r.GET("/protected", func(c *gin.Context) {
+		c.String(200, csrf.GetToken(c))
+	})
+
+	r.POST("/protected", func(c *gin.Context) {
+		c.String(200, "CSRF token is valid")
+	})
+
+	r.Run(":8000")
+}
